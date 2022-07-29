b5b3245c476767ab7ad7ae4696c14ebafc0bc790
Kiohi <ivancgutierrezkiohi@gmail.com>
2022-07-28 22:31:43 -0600

    Mobile menu from clase8.html added to index.html

--- mainscript.js
+++ mainscript.js
@@ -1,6 +1,12 @@
 const email = document.querySelector(".navbar-email");
-const menu = document.querySelector(".desktop-menu");
+const burgerMenu = document.querySelector(".menu");
+const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-email.addEventListener("click", () => {
-   menu.classList.toggle("inactive");
+var toggleMobileMenu = burgerMenu.addEventListener("click",() => {
+   mobileMenu.classList.toggle("inactive");
+});
+
+var toggleDesktopMenu = email.addEventListener("click", () => {
+   desktopMenu.classList.toggle("inactive");
 });
