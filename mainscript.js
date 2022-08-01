0205ccf909341c59ca9f2f7e4b453078c435511f
Kiohi <ivancgutierrezkiohi@gmail.com>
2022-07-31 23:52:06 -0600

    Aside/Shopping cart added to index.html from clase13.html

--- mainscript.js
+++ mainscript.js
@@ -2,11 +2,36 @@ const email = document.querySelector(".navbar-email");
 const burgerMenu = document.querySelector(".menu");
 const desktopMenu = document.querySelector(".desktop-menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const productDetail = document.querySelector(".product-detail");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+
 
 var toggleMobileMenu = burgerMenu.addEventListener("click",() => {
    mobileMenu.classList.toggle("inactive");
+   
+   if(!productDetail.classList.contains("inactive")){
+      productDetail.classList.add("inactive");
+   }
 });
 
-var toggleDesktopMenu = email.addEventListener("click", () => {
+var toggleEmailMenu = email.addEventListener("click", () => {
    desktopMenu.classList.toggle("inactive");
+
+   if(!productDetail.classList.contains("inactive")){
+      productDetail.classList.add("inactive");
+   }
 });
+
+var toggleDetails = shoppingCart.addEventListener("click", () => {
+   productDetail.classList.toggle("inactive");
+
+   if(!email.classList.contains("inactive")){
+      desktopMenu.classList.add("inactive");
+   }
+
+   if(!mobileMenu.classList.contains("inactive")){
+      mobileMenu.classList.add("inactive");
+   }
+
+   
+})
